eecb6cd6c2c6ebbc1d7f8099ca99a35ffc6dfaa5
MacaroniandBeans <macaroniandbeansgaming@gmail.com>
2025-08-09 08:24:12 -0500

    Update scripts-data.js

--- scripts-data.js
+++ scripts-data.js
@@ -1,20 +1,140 @@
 window.scripts = [
   {
-    title: "Cops & Robbers - FiveM",
-    screenshot: "assets/cops-robbers.png",
-    github: "https://github.com/MacaroniandBeans/cops-robbers",
-    video: "https://www.youtube.com/embed/your-video-id",
-    tech: ["Lua", "FiveM", "NUI", "JS"],
-    description: "A fast-paced, feature-rich Cops & Robbers mode for GTA V RP servers."
+    title: "Bank Truck Robbery",
+    screenshot: "assets/banktruck.png",
+    github: "https://github.com/MacaroniandBeans/beans-truckrobbery",
+    video: "",
+    tech: ["Strike System", "Global Cooldown", "XP system", "Highly Configurable"],
+    description: "A bank truck heist incorporating a strike system, penalty for death, guards, two ways to start the heist."
   },
   {
-    title: "Advanced Car HUD",
-    screenshot: "assets/car-hud.png",
+    title: "Arcade",
+    screenshot: "assets/arcade.png",
+    github: "https://github.com/MacaroniandBeans/beans-arcade",
+    video: "",
+    tech: ["QBCore", "Ox-Inventory", "Mini Games needed" ],
+    description: "Arcade system integrated with over 27 minigames. Token exchange system. A good money sink."
+  },
+    {
+    title: "Admin XP",
+    screenshot: "assets/adminxp.png",
+    github: "https://github.com/MacaroniandBeans/beans-adminxp",
+    video: "",
+    tech: ["QBCore", "SQL", "XP System"],
+    description: "An admin menu to add and remove xp. FUlly integrated with Pickle's XP."
+  },
+  {
+    title: "Bounty Hunter",
+    screenshot: "assets/bounties.png",
+    github: "https://github.com/MacaroniandBeans/beans-bounties",
+    video: "",
+    tech: ["XP System", "Highly Configurable"],
+    description: "Bounty Hunter missions. Integrated with Pickle's XP and AV-Gangs. XP requirement to start and can be configured to add to the Gang app in AV-Gangs"
+  },
+    {
+    title: "Car Play",
+    screenshot: "assets/carplay.png",
+    github: "https://github.com/MacaroniandBeans/beans-carplay",
+    video: "",
+    tech: ["OX Lib", "HTML/CSS", "Youtube Links"],
+    description: "Car Play for Youtube links. Has playlist fuctionality. Muffles sound when doors are closed with configurable sound distance"
+  },
+  {
+    title: "Drug Collecting",
+    screenshot: "assets/collecting.png",
+    github: "https://github.com/MacaroniandBeans/beanks-drugpicking",
+    video: "",
+    tech: ["Highly Customizable", "XP System", "HTML", "CSS"],
+    description: "Drug collecting script. Can configure the prop, XP, and locations."
+  },
+    {
+    title: "Drug Run",
+    screenshot: "assets/drugrun.png",
+    github: "https://github.com/MacaroniandBeans/beans-drugrun",
+    video: "",
+    tech: ["Highly Configurable", "AV-Drugs"],
+    description: "A plane mission with random amount of drop-offs. Uses Ox to make a stash. Integrated with AV Drugs and the payout is based on purity."
+  },
+  {
+    title: "Fleeca Heist",
+    screenshot: "assets/fleeca.png",
+    github: "https://github.com/MacaroniandBeans/beans-fleeca",
+    video: "",
+    tech: ["Strike System", "Global Cooldown", "XP System", "CSS"],
+    description: "Fleeca Heist with Strike system and XP. Global Cooldown integration. COnfigured for all Fleecas. Rare Item system based on Strikes."
+  },
+    {
+    title: "Global Cooldown",
+    screenshot: "assets/ccooldown.png",
+    github: "https://github.com/MacaroniandBeans/beans-globalutility",
+    video: "",
+    tech: ["Integrated with Scoreboard", "Highly Configurable", "Modular"],
+    description: "A global cooldown system. Integrated with the scoreboard script making it dynamic based on cooldown trigger."
+  },
+  {
+    title: "Train Heist",
+    screenshot: "assets/trainheist.png",
     github: "https://github.com/MacaroniandBeans/car-hud",
     video: "",
-    tech: ["Lua", "FiveM", "HTML", "CSS"],
-    description: "Customizable, minimal vehicle HUD for immersive gameplay."
-  }
+    tech: ["Ped Interaction", "Dialogue Interaction", "Highly Configurable", "Two Heists"],
+    description: "Train heist including Ped interaction with consequences. Timed heist."
+  },
+    {
+    title: "Modular Minigame System",
+    screenshot: "assets/minigames.png",
+    github: "https://github.com/MacaroniandBeans/beans-minigames",
+    video: "",
+    tech: ["Highly Configurable", "Modular", "XP System"],
+    description: "A modular minigame script. Integrated with Pickle's XP. Allows you to set difficulty based off of XP level and calls the export to provide the correct game."
+  },
+  {
+    title: "Gang Missions",
+    screenshot: "assets/gangmissions.png",
+    github: "https://github.com/MacaroniandBeans/beans-pettycrime",
+    video: "",
+    tech: ["XP System", "Gang integrated", "7 Missions"],
+    description: "7 gang missions. Drug lab bust up, Blackmail, Escorts, Porch Pirate, Power Syphon, Tailing, Turf War. Eahc game is unique with integrations to AV-Gangs for XP and triggerable by the app. "
+  },
+    {
+    title: "Pharmacy Robbery",
+    screenshot: "assets/pharmacy.png",
+    github: "https://github.com/MacaroniandBeans/beans-poppinpills",
+    video: "",
+    tech: ["Strike System", "Night/Daytime Robberies", "XP System", "requires MLO"],
+    description: "A pharmacy heist that has two different robberies based off of Day/Night cycle. Smash and Grab style or go in silent at night."
+  },
+  {
+    title: "Scoreboard",
+    screenshot: "assets/scoreboard.png",
+    github: "https://github.com/MacaroniandBeans/beans-scoreboard",
+    video: "",
+    tech: ["JS", "Integrated", "Highly Configurable"],
+    description: "Dynamic scoreboard when paired with the Cooldown script. Easily configure to yours. Add a custom image."
+  },
+    {
+    title: "Storage",
+    screenshot: "assets/storage.png",
+    github: "https://github.com/MacaroniandBeans/beans-storage",
+    video: "",
+    tech: ["MLO Needed", "Highly Configurable"],
+    description: "Storage script using OX and Kambi Storage Wars MLO. Can be configured to work with other locations/MLOs."
+  },
+    {
+    title: "Used Cars",
+    screenshot: "assets/usedcars.png",
+    github: "https://github.com/MacaroniandBeans/beans-usedcars",
+    video: "",
+    tech: ["SQL", "QBOX", "Highly Configurable $"],
+    description: "Used car script featuring configurable value. Also configured to work with Vin Scratch vehicles."
+  },
+    {
+    title: "Vector",
+    screenshot: "assets/vector.png",
+    github: "https://github.com/MacaroniandBeans/beans-vector",
+    video: "",
+    tech: ["Vector", "Commands"],
+    description: "Vector 3, Vector 4, Heading comands copied to clipboard. Because most admin menus suck."
+  },
   
 ];
 
